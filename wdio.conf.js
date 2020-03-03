8c88866bf0b40c789861a29c0e4bbe30311868e5
Nhataniel Manlangit <>
2020-03-03 22:31:50 +0800

    Docker services with common setup for selenium standalone

--- wdio.conf.js
+++ wdio.conf.js
@@ -149,8 +149,16 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['docker,', 'selenium-standalone'],
-
+    services: ['docker'],// 'selenium-standalone'],
+    dockerLogs: './',
+    dockerOptions: {
+        image: 'selenium/standalone-chrome',
+        healthCheck: 'http://localhost:4444',
+        options: {
+            p: ['4444:4444'],
+            shmSize: '2g'
+        }
+    },
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks.html
